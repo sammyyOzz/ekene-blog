1ee30c0a84475940771f86ac4b93c1a5d42420c4
Sammy <sammyoziegbe@gmail.com>
2024-06-14 00:24:15 +0100

    Initialize repository

---
+++ src/app/blog-details-two/[...id]/page.tsx
@@ -0,0 +1,26 @@
+import BlogDetailsArea from "@/components/blogs/blog-details/BlogDetailsArea";
+import Breadcrumbs from "@/components/common/Breadcrumbs";
+import inner_blog_data from "@/data/InnerBlogData";
+import Wrapper from "@/layouts/Wrapper";
+import FooterOne from "@/layouts/footers/FooterOne";
+import HeaderOne from "@/layouts/headers/HeaderOne";
+
+export const metadata = {
+   title: "Blog Details Zaira - News Magazine React Next Js Template",
+};
+const index = ({ params }: { params: { id: number } }) => {
+   const blogs = inner_blog_data.filter(items => items.page === "blog_2");
+   const single_blog = blogs.find((item) => Number(item.id) === Number(params.id));
+   return (
+      <Wrapper>
+         <HeaderOne />
+         <main className="fix">
+            <Breadcrumbs page="blogs" style={true} />
+            <BlogDetailsArea style={true} single_blog={single_blog} key={single_blog?.id}  />
+         </main>
+         <FooterOne style={false} style_2={true} />
+      </Wrapper>
+   )
+}
+
+export default index
